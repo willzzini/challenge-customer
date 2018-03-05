4f9416da5e0238f9d3591f698c09b5b8cfa44731
wilzzini <wilson.scavazzini@beblue.com.br>
2018-03-05 11:27:28 -0300

    treated error

--- src/Home.js
+++ src/Home.js
@@ -8,15 +8,17 @@ const Home = (props) => {
 			<TextField label="Informe seu CPF" helperText="Sem pontos" value={props.cpf} onChange={props.onChangeCPF} /> 
 			<div style={{ marginLeft: '15px' }}><Button variant="raised" color="primary" onClick={props.onSubmitCPF}>Consultar CPF</Button></div>
 			<Dialog open={props.showDialog} onClose={props.onCloseModal}>
+			<div className="message">
 			{props.success ? (
-			<div>
-			Seu nome está limpo!
-			</div>
-			) : (
 			<div>
 			Você tem restrições no seu nome!
 			</div>
+			) : (
+			<div>
+			Seu nome está limpo!
+			</div>
 			)}
+			</div>
 			</Dialog>
 		</div>
 	)
